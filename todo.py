f477411d48851266c4c7aa824c35c14660a243c9
laijoanne1997 <laijoanne1997@gmail.com>
2026-04-06 22:12:29 +0930

    adding json

--- todo.py
+++ todo.py
@@ -2,18 +2,28 @@
 #task tracker is a simple command line interface to track what needs to be done
 import datetime
 import random
+import json
 
 #should store tasks as a json file
 #should be able to add, delete, update, mark task, list all task, list all that are done, list all that are not done, list all in progress
 
-list = []
+def saving_file(task_list):
+    with open("tasks.json", "w") as file:
+        json.dump(task_list, file)
+    print("Tasks saved to file")
+
+def loading_file(task_list):
+    try:
+        with open("tasks.json", "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
 
 def adding_task(task_list, task, id="", description="", status="to do", created_at=datetime.datetime.now().strftime("%d %B %Y")):
     number = random.randint(1, 100)
     task_list.append({"task": task, "id": number, "description": description, "status": status, "created_at": created_at})
     print(f"{task} added to list (ID: {number} )")
     return task_list
-
 
 def update_description(task_list, task, description, updated=datetime.datetime.now().strftime("%d %B %Y")):
     for tasks in task_list:
