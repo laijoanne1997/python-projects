8ad56af5ff145933dd60ac5e10ced5711e786f19
laijoanne1997 <laijoanne1997@gmail.com>
2026-04-06 20:26:08 +0930

    adding description of the project

---
+++ todo.py
@@ -0,0 +1,11 @@
+#practising the task tracker project on roadmap
+#task tracker is a simple command line interface to track what needs to be done
+
+#should store tasks as a json file
+#should be able to add, delete, update, mark task, list all task, list all that are done, list all that are not done, list all in progress
+
+
+
+
+
+
