dfe8ab9513eb518f6b1b4e6fbab1af4117e441e3
laijoanne1997 <laijoanne1997@gmail.com>
2026-04-14 15:31:56 +0930

    updated interactions to work better

--- todo.py
+++ todo.py
@@ -7,14 +7,14 @@ import json
 #should store tasks as a json file
 #should be able to add, delete, update, mark task, list all task, list all that are done, list all that are not done, list all in progress
 
-def saving_file(task_list):
-    with open("tasks.json", "w") as file:
+def saving_file(task_list, filename="task.json"):
+    with open(filename, "w") as file:
         json.dump(task_list, file)
     print("Tasks saved to file")
 
-def loading_file(task_list):
+def loading_file(filename="tasks.json"):
     try:
-        with open("tasks.json", "r") as file:
+        with open(filename, "r") as file:
             return json.load(file)
     except FileNotFoundError:
         return []
@@ -44,7 +44,7 @@ def delete_task(task_list, task):
 def printing_tasks(task_list):
     for tasks in task_list:
         update = tasks.get("updated", "Not yet")
-        print(f"Task: {tasks['task']} | ID: {tasks["id"]} | Status: {tasks["status"]} | Description: {tasks['description']} | Created: {tasks["created_at"]} | Last updated: {update}")
+        print(f"\nTask: {tasks['task']} | ID: {tasks["id"]} | Status: {tasks["status"]} | Description: {tasks['description']} | Created: {tasks["created_at"]} | Last updated: {update}")
 
 def update_status(task_list, task, status, updated=datetime.datetime.now().strftime("%d %B %Y")):
     for tasks in task_list:
@@ -56,7 +56,7 @@ def update_status(task_list, task, status, updated=datetime.datetime.now().strft
 #up to creating a menu to add, update description or status, delete task, list all task, list by status
 
 def show_menu():
-    print("--Menu--")
+    print("\n--Menu--")
     print("1. Add task")
     print("2. Update task")
     print("3. Delete task")
@@ -73,29 +73,32 @@ while True:
         print ("Exiting...")
         break
     elif choice == 1:
-        task_name = input("Enter task name: ")
+        task_name = input("\nEnter task name: ")
         task_description = input("Enter task description: ")
-        adding_task(list, task_name, task_description)
+        adding_task(list, task_name, description=task_description)
         saving_file(list)
     elif choice == 2:
-        print("Do you want to update 1. Description or 2. Status")
+        print("\nDo you want to update 1. Description or 2. Status")
         pick = int(input("Enter your choice: "))
+        printing_tasks(list)
         if pick == 1:
-            task_name = input("Enter task name: ")
+            task_name = input("\nEnter task name: ")
             task_description = input("Enter task description: ")
-            update_status(list, task_name, task_description)
+            update_description(list, task_name, task_description)
             saving_file(list)
         elif pick == 2:
-            task_name = input("Enter task name: ")
+            task_name = input("\nEnter task name: ")
             task_status = input("Enter task status: ")
             update_status(list, task_name, task_status)
             saving_file(list)
     elif choice == 3:
         printing_tasks(list)
-        print("Which task would you like to delete?")
+        print("\nWhich task would you like to delete?")
         deleting_task = str(input("Enter the task: "))
         delete_task(list, deleting_task)
         saving_file(list)
     elif choice == 4:
         printing_tasks(list)
+    else:
+        print("Invalid choice. Please try again")
 
