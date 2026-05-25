8a02da5f0e8ad0e7b2446f43d4dd107d067b6143
laijoanne1997 <laijoanne1997@gmail.com>
2026-05-25 15:59:14 +0930

    end chapter 4

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,10 +1,9 @@
-from flask import render_template
+from flask import render_template, flasj, redirect, url_for
 from app import app
-from app.form import LoginForm
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
-
 def index():
     user = {'username': 'Miguel'}
     posts = [
@@ -24,5 +23,5 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me{}'.format(form.username.data, form.remember_me.data))
-        return redirect ('/index')
+        return redirect (url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
